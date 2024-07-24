c361429a38dcf8e7957575fc0d9492673dd2b7c0
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-07-24 15:42:04 +0200

    js

--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const handleIntersection = (entries, observer) => {
         entries.forEach(entry => {
-            const video = entry.target.querySelector('video.video');
+            const video = entry.target.querySelector('.video');
 
             if (entry.isIntersecting) {
                 video.play();
@@ -17,5 +17,43 @@ document.addEventListener('DOMContentLoaded', () => {
 
     videoPlayers.forEach(player => {
         observer.observe(player);
+        player.addEventListener("click", () => {
+            playPause()
+        })
     });
 });
+
+function playPause() {
+    const videoPlayers = document.querySelectorAll('.video-player');
+
+    var isPlaying = true;
+
+    videoPlayers.forEach(player => {
+
+        const video = player.querySelector('.video');
+
+        player.addEventListener("click", () => {
+            // if (isPlaying == true) {
+            //     video.pause();
+            //     isPlaying = false;
+            // } else if(isPlaying == false) {
+            //     video.play();
+            //     isPlaying = true
+            // }
+
+            switch (isPlaying) {
+                case true :
+                    video.pause()
+                    isPlaying = false;
+                    break;
+                
+                case false :
+                    video.play();
+                    isPlaying = true;
+                    break;
+            }
+        })
+
+    });
+        
+}
