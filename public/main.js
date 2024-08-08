21998afd55dc2876098abec7c856b37d487c4234
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-08-08 08:50:02 +0200

    TrippySavage

--- public/main.js
+++ public/main.js
@@ -1,59 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     const videoPlayers = document.querySelectorAll('.video-player');
 
-    const handleIntersection = (entries, observer) => {
-        entries.forEach(entry => {
-            const video = entry.target.querySelector('.video');
-
-            if (entry.isIntersecting) {
-                video.play();
-            }
-        });
-    };
-
-    const observer = new IntersectionObserver(handleIntersection, {
-        threshold: 0.5
-    });
-
-    videoPlayers.forEach(player => {
-        observer.observe(player);
-        player.addEventListener("click", () => {
-            playPause()
-        })
-    });
-});
-
-function playPause() {
-    const videoPlayers = document.querySelectorAll('.video-player');
-
     var isPlaying = true;
 
-    videoPlayers.forEach(player => {
+    videoPlayers.forEach(video =>{
+        const currentVideo = video.querySelector(".video");
 
-        const video = player.querySelector('.video');
-
-        player.addEventListener("click", () => {
-            // if (isPlaying == true) {
-            //     video.pause();
-            //     isPlaying = false;
-            // } else if(isPlaying == false) {
-            //     video.play();
-            //     isPlaying = true
-            // }
+        currentVideo.addEventListener("click", () => {
 
             switch (isPlaying) {
                 case true :
-                    video.pause()
-                    isPlaying = false;
-                    break;
+                   currentVideo.pause(); 
+                   isPlaying = false;
+                   break;
                 
                 case false :
-                    video.play();
-                    isPlaying = true;
-                    break;
+                   currentVideo.play(); 
+                   isPlaying = true;
+                   break;
             }
-        })
 
-    });
-        
-}
+        });
+    })
+    
+});
